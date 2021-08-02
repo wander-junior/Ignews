142346d4b0571db81fcaa224142b950f4b94d1e9
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-08-02 18:07:16 -0300

    Save user on database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query } from 'faunadb';
+
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,24 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: {email} }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
